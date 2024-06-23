b377377b49127e2e2e2ae07951b45215563af693
Ganesh GC <ganeshlema2021@gmail.com>
2024-06-23 13:59:53 -0500

    pagination and sorting implementation completed

--- src/main/java/com/ganeshgc/mobile_project/service/MobileNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ganeshgc.mobile_project.service;
-
-public class MobileNotFoundException extends RuntimeException{
-    public MobileNotFoundException(String message) {
-        super(message);
-    }
-}
